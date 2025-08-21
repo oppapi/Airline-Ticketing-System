eb634b4967b68bc64fc13897546e19433c62ad51
John Carl Fetalver Ola <johncarl4102@gmail.com>
2025-08-21 23:17:46 +0800

    UPDATE admin ui

--- Airline Ticketing System/adminLogin.Designer.cs
+++ Airline Ticketing System/adminLogin.Designer.cs
@@ -28,69 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtDate = new System.Windows.Forms.TextBox();
-            this.btnTicket = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtTicketNum = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(-198, 181);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(852, 258);
-            this.dataGridView1.TabIndex = 47;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Quicksand", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(60)))));
-            this.label6.Location = new System.Drawing.Point(295, 93);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(52, 25);
-            this.label6.TabIndex = 46;
-            this.label6.Text = "Date";
-            // 
-            // txtDate
-            // 
-            this.txtDate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(248)))), ((int)(((byte)(254)))));
-            this.txtDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtDate.Font = new System.Drawing.Font("Quicksand SemiBold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(60)))));
-            this.txtDate.Location = new System.Drawing.Point(300, 121);
-            this.txtDate.Name = "txtDate";
-            this.txtDate.Size = new System.Drawing.Size(186, 39);
-            this.txtDate.TabIndex = 45;
-            this.txtDate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // btnTicket
-            // 
-            this.btnTicket.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(60)))));
-            this.btnTicket.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(60)))));
-            this.btnTicket.Font = new System.Drawing.Font("Quicksand", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTicket.ForeColor = System.Drawing.Color.White;
-            this.btnTicket.Location = new System.Drawing.Point(434, 458);
-            this.btnTicket.Margin = new System.Windows.Forms.Padding(4);
-            this.btnTicket.Name = "btnTicket";
-            this.btnTicket.Size = new System.Drawing.Size(220, 54);
-            this.btnTicket.TabIndex = 44;
-            this.btnTicket.Text = "REFRESH";
-            this.btnTicket.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Quicksand", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(60)))));
-            this.label2.Location = new System.Drawing.Point(-203, 93);
+            this.label2.Location = new System.Drawing.Point(-3, 149);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(132, 25);
             this.label2.TabIndex = 43;
@@ -102,7 +50,7 @@
             this.txtTicketNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtTicketNum.Font = new System.Drawing.Font("Quicksand SemiBold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTicketNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(60)))));
-            this.txtTicketNum.Location = new System.Drawing.Point(-198, 121);
+            this.txtTicketNum.Location = new System.Drawing.Point(2, 177);
             this.txtTicketNum.Name = "txtTicketNum";
             this.txtTicketNum.Size = new System.Drawing.Size(377, 39);
             this.txtTicketNum.TabIndex = 42;
@@ -112,7 +60,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Quicksand", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(60)))));
-            this.label1.Location = new System.Drawing.Point(97, 43);
+            this.label1.Location = new System.Drawing.Point(92, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(262, 43);
             this.label1.TabIndex = 41;
@@ -122,29 +70,20 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(232)))));
             this.ClientSize = new System.Drawing.Size(451, 554);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.txtDate);
-            this.Controls.Add(this.btnTicket);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtTicketNum);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "adminLogin";
             this.Text = "adminLogin";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtDate;
-        private System.Windows.Forms.Button btnTicket;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtTicketNum;
         private System.Windows.Forms.Label label1;
